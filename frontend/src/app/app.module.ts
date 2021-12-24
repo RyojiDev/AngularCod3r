571355ed300b952bab85622b1da7ad2d547f5667
Ryoji Kitano <ryojikitanodev@gmail.com>
2021-12-24 07:19:14 -0300

    rename the directive directory to directives

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -16,7 +16,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
-import { RedDirective } from './directive/red.directive';
+import { RedDirective } from './directives/red.directive';
 
 @NgModule({
   declarations: [
